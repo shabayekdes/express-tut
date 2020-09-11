d9ddfcbf55e8b779db9598418b0fba14e54039e6
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-09-11 22:15:06 +0200

    Add middleware and static public file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -8,8 +9,30 @@ app.listen(3000)
 // register view engine
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'))
+
 // if you want change views directory
 //app.set('views', 'myViews');
+
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('in the next middleware');
+    next();
+  });
+  
+  app.use(morgan('dev'));
+  
+  app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+  });
 
 app.get('/', (req, res) => {
     const blogs = [
