da4564505c3da63e3f92bdc321df03647067b3a0
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-11-01 15:52:03 +0200

    Add login form

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,16 @@ const jwt = require('jsonwebtoken');
 const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email: '', password: '' };
+
+    // incorrect email
+    if (err.message === 'incorrect email') {
+        errors.email = 'That email is not registered';
+    }
+
+    // incorrect password
+    if (err.message === 'incorrect password') {
+        errors.password = 'That password is incorrect';
+    }
   
     // duplicate email error
     if (err.code === 11000) {
@@ -63,6 +73,16 @@ module.exports.signup_post = async (req, res) => {
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
 
-    console.log(email, password);
-    res.send('user login');
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(200).json({ user: user._id });
+
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+
+    }
+
 } 
